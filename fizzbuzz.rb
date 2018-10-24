90968123c72b10d428684b66f476a735dfc93e5c
Cory Adams <coadams9@gmail.com>
2018-10-24 11:22:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if 
-    return "Fizz"
-  elsif int % 5 == 0
-    return "Buzz"
-  else 
+  if (int % 3 == 0 && int % 5 == 0)
     return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  else int % 5 == 0
+    return "Buzz"
   end
 end
